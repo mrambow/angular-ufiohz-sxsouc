84bbcc5f05048107fe588f39f99b10e51d6e1ba3
Marc Rambow <marc.rambow@sinc.de>
2022-08-24 22:40:43 +0200

    adding custom stuff

--- src/app/example-tel-input/example-tel-input-example.ts
+++ src/app/example-tel-input/example-tel-input-example.ts
@@ -1,172 +1,277 @@
 import { FocusMonitor } from '@angular/cdk/a11y';
 import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
-import {
-  Component,
-  ElementRef,
-  Inject,
-  Input,
-  OnDestroy,
-  Optional,
-  Self,
-  ViewChild,
-} from '@angular/core';
-import {
-  AbstractControl,
-  ControlValueAccessor,
-  FormBuilder,
-  NgControl,
-  Validators,
-} from '@angular/forms';
-import {
-  MatFormField,
-  MatFormFieldControl,
-  MAT_FORM_FIELD,
-} from '@angular/material/form-field';
+import { Component, ElementRef, Inject, Input, OnDestroy, Optional, QueryList, Self, ViewChildren } from '@angular/core';
+import { AbstractControl, ControlValueAccessor, FormBuilder, FormControl, NgControl, Validators } from '@angular/forms';
+import { MAT_FORM_FIELD, MatFormField, MatFormFieldControl } from '@angular/material/form-field';
 import { Subject } from 'rxjs';
 
-/** Data structure for holding telephone number. */
-export class MyTel {
-  constructor(
-    public area: string,
-    public exchange: string,
-    public subscriber: string
-  ) {}
+export class PinCode {
+  constructor( public pin: string ) {
+  }
+
+  getSingleDigits(): string[] {
+    return this.pin ? this.pin.split( '' ) : [
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+    ];
+  }
 }
 
 /** Custom `MatFormFieldControl` for telephone number input. */
-@Component({
+@Component( {
   selector: 'example-tel-input',
   templateUrl: 'example-tel-input-example.html',
   styleUrls: ['example-tel-input-example.css'],
-  providers: [{ provide: MatFormFieldControl, useExisting: MyTelInput }],
+  providers: [
+    {
+      provide: MatFormFieldControl,
+      useExisting: MyTelInput,
+    },
+  ],
   host: {
     '[class.example-floating]': 'shouldLabelFloat',
     '[id]': 'id',
   },
-})
-export class MyTelInput
-  implements ControlValueAccessor, MatFormFieldControl<MyTel>, OnDestroy
-{
-  static nextId = 0;
-  @ViewChild('area') areaInput: HTMLInputElement;
-  @ViewChild('exchange') exchangeInput: HTMLInputElement;
-  @ViewChild('subscriber') subscriberInput: HTMLInputElement;
+} )
+export class MyTelInput implements ControlValueAccessor, MatFormFieldControl<PinCode>, OnDestroy {
 
-  parts = this._formBuilder.group({
-    area: [
-      '',
-      [Validators.required, Validators.minLength(3), Validators.maxLength(3)],
-    ],
-    exchange: [
-      '',
-      [Validators.required, Validators.minLength(3), Validators.maxLength(3)],
-    ],
-    subscriber: [
-      '',
-      [Validators.required, Validators.minLength(4), Validators.maxLength(4)],
-    ],
-  });
+  private static nextId = 0;
   stateChanges = new Subject<void>();
   focused = false;
   touched = false;
   controlType = 'example-tel-input';
   id = `example-tel-input-${MyTelInput.nextId++}`;
-  onChange = (_: any) => {};
-  onTouched = () => {};
+  @Input( 'aria-describedby' ) userAriaDescribedBy: string;
+  @ViewChildren( 'singleDigitInput' ) digitInputs: QueryList<ElementRef<HTMLInputElement>>;
+  partsForm = this._formBuilder.group( MyTelInput.createFormControlsForPin() );
+  autofilled?: boolean;
 
-  get empty() {
-    const {
-      value: { area, exchange, subscriber },
-    } = this.parts;
-
-    return !area && !exchange && !subscriber;
+  constructor( private _formBuilder: FormBuilder,
+    private _focusMonitor: FocusMonitor,
+    private _elementRef: ElementRef<HTMLElement>,
+    @Optional() @Inject( MAT_FORM_FIELD ) public _formField: MatFormField,
+    @Optional() @Self() public ngControl: NgControl ) {
+    if ( this.ngControl != null ) {
+      this.ngControl.valueAccessor = this;
+    }
   }
 
   get shouldLabelFloat() {
     return this.focused || !this.empty;
   }
 
-  @Input('aria-describedby') userAriaDescribedBy: string;
+  get errorState(): boolean {
+    return this.partsForm.invalid && this.touched;
+  }
 
-  @Input()
-  get placeholder(): string {
+  get formKeys(): string[] {
+    return Object.keys( this.partsForm.controls );
+  }
+
+  get empty(): boolean {
+    const {
+      value: {
+        pin0,
+        pin1,
+        pin2,
+        pin3,
+        pin4,
+        pin5,
+      },
+    } = this.partsForm;
+
+    return !pin0 && !pin1 && !pin2 && !pin3 && !pin4 && !pin5;
+  }
+
+  private _placeholder: string;
+
+  @Input() get placeholder(): string {
     return this._placeholder;
   }
-  set placeholder(value: string) {
+
+  set placeholder( value: string ) {
     this._placeholder = value;
     this.stateChanges.next();
   }
-  private _placeholder: string;
 
-  @Input()
-  get required(): boolean {
-    return this._required;
-  }
-  set required(value: BooleanInput) {
-    this._required = coerceBooleanProperty(value);
-    this.stateChanges.next();
-  }
   private _required = false;
 
-  @Input()
-  get disabled(): boolean {
-    return this._disabled;
+  @Input() get required(): boolean {
+    return this._required;
   }
-  set disabled(value: BooleanInput) {
-    this._disabled = coerceBooleanProperty(value);
-    this._disabled ? this.parts.disable() : this.parts.enable();
+
+  set required( value: BooleanInput ) {
+    this._required = coerceBooleanProperty( value );
     this.stateChanges.next();
   }
+
   private _disabled = false;
 
-  @Input()
-  get value(): MyTel | null {
-    if (this.parts.valid) {
-      const {
-        value: { area, exchange, subscriber },
-      } = this.parts;
-      return new MyTel(area!, exchange!, subscriber!);
-    }
-    return null;
+  @Input() get disabled(): boolean {
+    return this._disabled;
   }
-  set value(tel: MyTel | null) {
-    const { area, exchange, subscriber } = tel || new MyTel('', '', '');
-    this.parts.setValue({ area, exchange, subscriber });
+
+  set disabled( value: BooleanInput ) {
+    this._disabled = coerceBooleanProperty( value );
+    if ( this._disabled ) {
+      this.partsForm.disable();
+    } else {
+      this.partsForm.enable();
+    }
     this.stateChanges.next();
   }
 
-  get errorState(): boolean {
-    return this.parts.invalid && this.touched;
-  }
-
-  constructor(
-    private _formBuilder: FormBuilder,
-    private _focusMonitor: FocusMonitor,
-    private _elementRef: ElementRef<HTMLElement>,
-    @Optional() @Inject(MAT_FORM_FIELD) public _formField: MatFormField,
-    @Optional() @Self() public ngControl: NgControl
-  ) {
-    if (this.ngControl != null) {
-      this.ngControl.valueAccessor = this;
+  @Input() get value(): PinCode {
+    if ( this.partsForm.valid ) {
+      const {
+        value: {
+          pin0,
+          pin1,
+          pin2,
+          pin3,
+          pin4,
+          pin5,
+        },
+      } = this.partsForm;
+      return new PinCode( [
+        pin0,
+        pin1,
+        pin2,
+        pin3,
+        pin4,
+        pin5,
+      ].join( '' ) );
     }
+    return new PinCode( '' );
   }
 
-  ngOnDestroy() {
-    this.stateChanges.complete();
-    this._focusMonitor.stopMonitoring(this._elementRef);
+  set value( pinCode: PinCode ) {
+    const [pin0, pin1, pin2, pin3, pin4, pin5] = ( pinCode || new PinCode( '' ) ).getSingleDigits();
+    this.partsForm.setValue( {
+      pin0,
+      pin1,
+      pin2,
+      pin3,
+      pin4,
+      pin5,
+    } );
+    // this.touched = true;
+    this.stateChanges.next();
   }
 
-  onFocusIn(event: FocusEvent) {
-    if (!this.focused) {
+  private static createFormControlsForPin( pinDigits = 6 ): Record<string, FormControl> {
+    const formControls: Record<string, FormControl> = {};
+
+    for ( let i = 0; i < pinDigits; i++ ) {
+      formControls[`pin${i}`] = new FormControl(
+        '',
+        [
+          Validators.required,
+          Validators.minLength( 1 ),
+          Validators.maxLength( 1 ),
+        ] );
+    }
+    return formControls;
+  }
+
+  onFocusIn( event: FocusEvent ) {
+    if ( !this.focused ) {
       this.focused = true;
       this.stateChanges.next();
     }
   }
 
-  onFocusOut(event: FocusEvent) {
-    if (
-      !this._elementRef.nativeElement.contains(event.relatedTarget as Element)
-    ) {
+  autoFocusPrev( control: AbstractControl, prevElement: HTMLInputElement ): void {
+    if ( control.value.length < 1 ) {
+      this._focusMonitor.focusVia(
+        prevElement,
+        'program' );
+    }
+  }
+
+  setDescribedByIds( ids: string[] ) {
+    try {
+      const controlElement = this._elementRef.nativeElement.querySelector( '.example-tel-input-container' )!;
+      controlElement.setAttribute(
+        'aria-describedby',
+        ids.join( ' ' ) );
+    } catch {
+    }
+  }
+
+  onContainerClick( event: MouseEvent ) {
+    if ( ( event.target as Element ).tagName.toLowerCase() !== 'input' ) {
+      this.digitInputs.first.nativeElement.focus();
+    }
+  }
+
+  registerOnChange( fn: any ): void {
+    this.onChange = fn;
+  }
+
+  // eslint-disable-next-line
+  onChange = ( _: any ): void => {
+  };
+
+  // eslint-disable-next-line
+  onTouched = () => {
+  };
+
+  ngOnDestroy(): void {
+    this.stateChanges.complete();
+    this._focusMonitor.stopMonitoring( this._elementRef );
+  }
+
+  pasteDataHandler( event: ClipboardEvent ): void {
+    if ( event.clipboardData === null ) {
+      return;
+    }
+
+    const pinFromClipboard = event.clipboardData.getData( 'text' )
+      .trim();
+    if ( !/\^d{6}$/.test( pinFromClipboard ) ) {
+      return event.preventDefault();
+    }
+    this.value = new PinCode( pinFromClipboard );
+    this.digitInputs.last.nativeElement.focus();
+    this.stateChanges.next();
+  }
+
+  inputDataHandler( event: Event ): void {
+    if ( event instanceof InputEvent ) {
+      if ( event.inputType === 'deleteContentBackward' ) {
+        return;
+      }
+
+      const indexOfCurrentInput = Number( ( event.target as HTMLInputElement ).dataset.index );
+      const nextElement = this.digitInputs.get( indexOfCurrentInput + 1 );
+      if ( nextElement ) {
+        nextElement.nativeElement.focus();
+      } else {
+        this.onChange( this.value );
+        this.stateChanges.next();
+      }
+    }
+  }
+
+  removeDigitsHandler( event: Event ): void {
+    setTimeout(
+      () => {
+        const i = Number( ( event.target as HTMLInputElement ).dataset.index );
+        this.digitInputs.get( i - 1 )
+          ?.nativeElement
+          .focus();
+      },
+      100 );
+  }
+
+  // eslint-disable-next-line
+  onFocusOut( event: FocusEvent ) {
+    if ( !this._elementRef.nativeElement.contains( event.relatedTarget as Element ) ) {
       this.touched = true;
       this.focused = false;
       this.onTouched();
@@ -174,62 +279,27 @@ export class MyTelInput
     }
   }
 
-  autoFocusNext(
-    control: AbstractControl,
-    nextElement?: HTMLInputElement
-  ): void {
-    if (!control.errors && nextElement) {
-      this._focusMonitor.focusVia(nextElement, 'program');
+  autoFocusNext( control: AbstractControl, nextElement?: HTMLInputElement ): void {
+    if ( !control.errors && nextElement ) {
+      this._focusMonitor.focusVia(
+        nextElement,
+        'program' );
     }
   }
 
-  autoFocusPrev(control: AbstractControl, prevElement: HTMLInputElement): void {
-    if (control.value.length < 1) {
-      this._focusMonitor.focusVia(prevElement, 'program');
-    }
-  }
-
-  setDescribedByIds(ids: string[]) {
-    const controlElement = this._elementRef.nativeElement.querySelector(
-      '.example-tel-input-container'
-    )!;
-    controlElement.setAttribute('aria-describedby', ids.join(' '));
-  }
-
-  onContainerClick() {
-    if (this.parts.controls.subscriber.valid) {
-      this._focusMonitor.focusVia(this.subscriberInput, 'program');
-    } else if (this.parts.controls.exchange.valid) {
-      this._focusMonitor.focusVia(this.subscriberInput, 'program');
-    } else if (this.parts.controls.area.valid) {
-      this._focusMonitor.focusVia(this.exchangeInput, 'program');
-    } else {
-      this._focusMonitor.focusVia(this.areaInput, 'program');
-    }
-  }
-
-  writeValue(tel: MyTel | null): void {
+  writeValue( tel: PinCode ): void {
     this.value = tel;
   }
 
-  registerOnChange(fn: any): void {
-    this.onChange = fn;
-  }
-
-  registerOnTouched(fn: any): void {
+  registerOnTouched( fn: never ): void {
     this.onTouched = fn;
   }
 
-  setDisabledState(isDisabled: boolean): void {
+  setDisabledState( isDisabled: boolean ): void {
     this.disabled = isDisabled;
   }
 
-  _handleInput(control: AbstractControl, nextElement?: HTMLInputElement): void {
-    this.autoFocusNext(control, nextElement);
-    this.onChange(this.value);
+  ariaLabel( index: number ): string {
+    return index === 0 ? 'Bitte geben Sie den PIN ein. -stellig 1' : '-stellig ' + ( index + 1 );
   }
 }
-
-/**  Copyright 2022 Google LLC. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at https://angular.io/license */
--- src/app/form-field-custom-control-example.ts
+++ src/app/form-field-custom-control-example.ts
@@ -1,46 +1,23 @@
-import { FocusMonitor } from '@angular/cdk/a11y';
-import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
-import {
-  Component,
-  OnInit,
-  ElementRef,
-  Inject,
-  Input,
-  OnDestroy,
-  Optional,
-  Self,
-  ViewChild,
-} from '@angular/core';
-import {
-  AbstractControl,
-  ControlValueAccessor,
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  NgControl,
-  Validators,
-} from '@angular/forms';
-import {
-  MAT_FORM_FIELD,
-  MatFormField,
-  MatFormFieldControl,
-} from '@angular/material/form-field';
-import { Subject } from 'rxjs';
-import { MyTel } from './example-tel-input/example-tel-input-example';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { PinCode } from './example-tel-input/example-tel-input-example';
 
 /** @title Form field with custom telephone number input control. */
-@Component({
+@Component( {
   selector: 'form-field-custom-control-example',
   templateUrl: 'form-field-custom-control-example.html',
-})
+} )
 export class FormFieldCustomControlExample implements OnInit {
-  form: FormGroup = new FormGroup({
-    tel: new FormControl(new MyTel('', '', '')),
-  });
+  form: FormGroup = new FormGroup( {
+    tel: new FormControl( new PinCode( '' ) ),
+  } );
 
   ngOnInit() {
-    setTimeout(() => {
-      console.log('aa');
-    }, 2000);
+    setTimeout(
+      () => {
+        this.form.get( 'tel' )
+          ?.setErrors( { 'invalid': true } );
+      },
+      2000 );
   }
 }
